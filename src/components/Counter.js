9add45f7d20ff6e5f269be5b5b033b882846ddc5
Laura Daugherty <lauradaugherty6@gmail.com>
2019-06-17 15:12:19 -0700

    MVP and 1 stretch done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
+
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if (this.props.count % 2 !== 0) {
+            this.props.increment()
+        }
     };
 
     incrementAsync = () => {
@@ -20,20 +24,20 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { this.props.increment() }}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { this.props.decrement() }}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
+                </button> 
+                {/* <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>   */}
             </p>
         );
     }
@@ -50,6 +54,7 @@ const mapStateToProps = (state) => {
         count: state.count
     };
 };
+ 
 
 // The connect function is called in order to make this component aware
 // of the rest of the redux architecture. Without this, this component
